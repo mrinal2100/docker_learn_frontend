08d311cd49bf3c936bcb801cafa8b3fffc949174
mrinal2100 <mrinals2100@gmail.com>
2025-12-25 03:45:01 -0500

    added to src

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        hello  Mrinal Singh  
+        hello  Mrinal Singh from Patna
 	Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
