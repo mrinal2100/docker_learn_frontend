67f891ecc5bdf4b27394dff10b9074af2be59bcf
mrinal2100 <mrinals2100@gmail.com>
2025-12-25 05:25:31 -0500

    chage to Dockerfile for production env

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        I was changed on feature
+        I was changed on feature...
 	Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
